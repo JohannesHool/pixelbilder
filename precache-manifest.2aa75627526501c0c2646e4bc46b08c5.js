22249d558416e1188e123c5692406ea480349105
JohannesHool <hooljohannes@gmail.com>
2020-12-31 16:13:22 +0100

    Updates

---
+++ precache-manifest.2aa75627526501c0c2646e4bc46b08c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d76104af017ee870aaffd637d4277c",
+    "url": "/pixelbilder/index.html"
+  },
+  {
+    "revision": "806d326443f092eb7ac2",
+    "url": "/pixelbilder/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "4b5f2d238bd5d0427c07",
+    "url": "/pixelbilder/static/css/main.6be970a7.chunk.css"
+  },
+  {
+    "revision": "806d326443f092eb7ac2",
+    "url": "/pixelbilder/static/js/2.1e39b9cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixelbilder/static/js/2.1e39b9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5f2d238bd5d0427c07",
+    "url": "/pixelbilder/static/js/main.5b18d2a2.chunk.js"
+  },
+  {
+    "revision": "c70c5fe479cb1e26f923",
+    "url": "/pixelbilder/static/js/runtime-main.2db1472c.js"
+  }
+]);
